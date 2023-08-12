4618b6a454c59362024195ec9f62c76b8835a70a
Uzair Aslam <uzairaslam990@gmail.com>
2023-08-12 01:15:23 -0700

    counter-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from "react";
 function App() {
+  var userName="Ali"
+  var [count, setCount] = useState(0);
+  const minimize = () => {
+    setCount(--count);
+  };
+
+  const maximize = () => {
+    setCount(++count);
+  };
+  const Reset =()=>{
+    setCount(0);
+  }
+  console.log("userName in console", userName);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center'>
+      <h1 className='text-center'>Counter App </h1>
+      <div className='mt-5'>
+      <button className='btn btn-primary btn-lg mx-auto' style={{paddingLeft:"20px",paddingRight:"20px"}} onClick={minimize}>-</button>
+      <span className='p-3 h3 '>{count}</span>
+      <button className='btn btn-primary btn-lg mx-auto' style={{paddingLeft:"20px",paddingRight:"20px"}} onClick={maximize}>+</button><br/>
+      <button className='btn btn-dark btn-lg mx-auto mt-5' onClick={Reset}>Reset All</button>
+      </div>
     </div>
   );
 }
